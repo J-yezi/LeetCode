e77d3b65695b1f958adb2cb9a432015da5949fca
叶浩 <yehao@xiehaodeMac-mini.local>
2019-04-24 15:17:47 +0800

    上传

--- 001_Two_Sum.py
+++ 001_Two_Sum.py
@@ -1,5 +1,11 @@
+'''
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标
+'''
+
 class Solution:
-    # 利用两次for循环处理
+    '''
+    利用两次for循环处理
+    '''
     # def twoSum(self, nums, target):
     #     for i, num_i in enumerate(nums):
     #         for j, num_j in enumerate(nums):
@@ -7,13 +13,16 @@ class Solution:
     #                 return [i, j]
     #     return None
 
-    # 对上面方法进行改进，利用hashmap来节省一次for循环
+    '''
+    利用map来存储之前的元素，当碰到当前元素和目标元素=target的时候，就找到了
+    相比较两次for循环来说，时间复杂度下降了，利用map来进行元素的筛选会比list进行for要快
+    '''
     def twoSum(self, nums, target):
         hashmap = {}
         for index, num in enumerate(nums):
-            aother = target - num
-            if aother in hashmap:
-                return [hashmap[aother], index]
+            another = target - num
+            if another in hashmap:
+                return [hashmap[another], index]
             hashmap[num] = index
         return None
 
---
+++ 003_Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,47 @@
+'''
+给定一个字符串，请你找出其中不含有重复字符的最长子串的长度
+'''
+
+import time
+
+class Solution(object):
+    # def lengthOfLongestSubstring(self, s):
+    #     value = 0
+    #     keys = set()
+    #     ls = len(s)
+    #     for i in range(ls):
+    #         temp = 0
+    #         keys.clear()
+    #         for j in range(i, ls):
+    #             if not s[j] in keys:
+    #                 keys.add(s[j])
+    #                 temp += 1
+    #             else: break
+    #         value = max(value, temp)
+    #     return value
+
+    '''
+    从0到j处理字符串，而不是从j到len(s)
+    利用st来记录，上一个重复字符串的位置(i = max(st[s[j]], i))
+    i的目的是记录当前字符最近一次重复字符的位置(不一定是当前字符重复，可能是其他字符重复)
+
+    利用map来记录当前字符上一次出现位置，然后在找到这段字符串当中还有没有其他字符的上一次重复位置i
+    这样保证了，从当前字符到i这个位置的字符串中是不存在重复字符的，然后使用ans来记录最大值
+    '''
+    def lengthOfLongestSubstring(self, s):
+        st = {}
+        i, ans = 0, 0
+        for j in range(len(s)):
+            if s[j] in st:
+                i = max(st[s[j]], i)
+            ans = max(ans, j - i + 1)
+            st[s[j]] = j + 1
+        return ans
+
+
+if __name__ == '__main__':
+    s = Solution()
+    start = time.time()
+    print(s.lengthOfLongestSubstring("abba"))
+    end = time.time()
+    print((end - start) * 1000)
