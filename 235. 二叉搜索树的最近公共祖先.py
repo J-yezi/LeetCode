dd2db25800bafa76b24dc73ff5f79261a7b7c34f
yehao <yehao1020@gmail.com>
2019-11-15 10:34:37 +0800

    添加新算法

---
+++ 235. 二叉搜索树的最近公共祖先.py
@@ -0,0 +1,67 @@
+#coding=utf-8
+
+'''
+给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+
+示例 1:
+输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+输出: 6 
+解释: 节点 2 和节点 8 的最近公共祖先是 6。
+
+示例 2:
+输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+输出: 2
+解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+'''
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # 递归
+    # def lowestCommonAncestor(self, root, p, q):
+    #     if p.val > root.val and q.val > root.val:
+    #         return self.lowestCommonAncestor(root.right, p, q)
+    #     elif p.val < root.val and q.val < root.val:
+    #         return self.lowestCommonAncestor(root.left, p, q)
+    #     else:
+    #         return root
+
+    # 迭代
+    def lowestCommonAncestor(self, root, p, q):
+        while root is not None:
+            if p.val > root.val and q.val > root.val:
+                root = root.right
+            elif p.val < root.val and q.val < root.val:
+                root = root.left
+            else:
+                return root
+
+if __name__ == "__main__":
+    node1 = TreeNode(6)
+    node2 = TreeNode(2)
+    node3 = TreeNode(8)
+    node4 = TreeNode(0)
+    node5 = TreeNode(4)
+    node6 = TreeNode(7)
+    node7 = TreeNode(9)
+    node8 = TreeNode(3)
+    node9 = TreeNode(5)
+
+    node1.left = node2
+    node1.right = node3
+    node2.left = node4
+    node2.right = node5
+    node5.left = node8
+    node5.right = node9
+    node3.left = node6
+    node3.right = node7
+
+    s = Solution()
+    print(s.lowestCommonAncestor(node1, node4, node9).val)
+    pass
