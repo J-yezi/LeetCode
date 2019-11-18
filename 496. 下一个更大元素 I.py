0b5cad32a684ce654a16d814a7b1604d670b6d5d
yehao <yehao1020@gmail.com>
2019-11-18 11:28:37 +0800

    添加新算法

---
+++ 496. 下一个更大元素 I.py
@@ -0,0 +1,47 @@
+#coding=utf-8
+
+'''
+给定两个没有重复元素的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。找到 nums1 中每个元素在 nums2 中的下一个比其大的值。
+
+nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出-1。
+
+示例 1:
+
+输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+输出: [-1,3,-1]
+解释:
+    对于num1中的数字4，你无法在第二个数组中找到下一个更大的数字，因此输出 -1。
+    对于num1中的数字1，第二个数组中数字1右边的下一个较大数字是 3。
+    对于num1中的数字2，第二个数组中没有下一个更大的数字，因此输出 -1。
+'''
+
+'''
+我们维护了一个单调栈，栈中的元素从栈顶到栈底是单调不降的。当我们遇到一个新的元素 nums2[i] 时，我们判断栈顶元素是否小于 nums2[i]，如果是，那么栈顶元素的下一个更大元素即为 nums2[i]，我们将栈顶元素出栈。重复这一操作，直到栈为空或者栈顶元素大于 nums2[i]。此时我们将 nums2[i] 入栈，保持栈的单调性，并对接下来的 nums2[i + 1], nums2[i + 2] ...
+
+分析它的时间复杂度，要从整体来看：总共有 n 个元素，每个元素都被 push 入栈了一次，而最多会被 pop 一次，没有任何冗余操作。所以总的计算规模是和元素规模 n 成正比的，也就是 O(n) 的复杂度
+'''
+
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        stack, dic = [], {}
+        for i in nums2:
+            if len(stack) == 0:
+                stack.append(i)
+            else:
+                while len(stack) > 0 and stack[-1] < i:
+                    last = stack.pop()
+                    dic[last] = i
+                stack.append(i)
+
+        arr = []
+        for i in nums1:
+            if i in dic:
+                arr.append(dic[i])
+            else:
+                arr.append(-1)
+        return arr
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.nextGreaterElement([2,4], [1,2,3,4]))
